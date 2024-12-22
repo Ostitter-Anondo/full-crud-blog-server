3b3c1edff789e05e209abf96c9d2d5b532619dd9
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2024-12-22 18:02:15 +0600

    mongodb connection setup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Batch 10 Assignment 11 by Jawad Ibn Mamoon
+
 - this is the server side repo for the assignment
 - assignment category: 05
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config();
+
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,10 +10,34 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// mongo setup
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aye3q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const database = client.db("blogSite");
+
+const userCol = database.collection("users");
+const blogCol = database.collection("blogs");
+const commentCol = database.collection("comments");
+
+// general landing
+
 app.get("/", (req, res) => {
-  res.send(`all your blogses are belong to us`);
+  res.send(
+    `all your blogses are belong to us. mongodb server is currently functional.`
+  );
 });
 
 app.listen(port, () => {
   console.log(`we having stuffs happun at ${port}`);
 });
+
+// mongodb stuff
