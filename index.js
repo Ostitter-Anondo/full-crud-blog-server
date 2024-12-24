715443eaadb1707879bca2ca4b2e9e17ac2bc42b
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2024-12-24 23:36:22 +0600

    add and remove from wihslist calls

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const client = new MongoClient(uri, {
 const database = client.db("blogSite");
 
 const userCol = database.collection("users");
+const wishlistCol = database.collection("wishlist");
 const blogCol = database.collection("blogs");
 const commentCol = database.collection("comments");
 
@@ -55,13 +56,22 @@ app.listen(port, () => {
 app.get("/user/:id", async (req, res) => {
   const query = { uid: req.params.id };
   const user = await userCol.findOne(query);
-  res.send(user);
+  const wishlist = await wishlistCol.findOne(query);
+  res.send({ user, wishlist });
 });
 
 // signup call
 
 app.post("/newuser", async (req, res) => {
-  const newUser = req.body;
+  const newWishlist = { uid: req.body.uid, wishlist: [] };
+  const wishlistData = await wishlistCol.insertOne(newWishlist);
+  const newUser = {
+    uid: req.body.uid,
+    email: req.body.email,
+    name: req.body.name,
+    photo: req.body.photo,
+    wishlistId: wishlistData.insertedId,
+  };
   console.log(`creating new user with data`, newUser);
   const result = await userCol.insertOne(newUser);
   res.send(result);
@@ -76,7 +86,8 @@ app.put("/googleuser", async (req, res) => {
   console.log(`new google login happens`);
   const result = await userCol.updateOne(filter, updatedUser, options);
   const user = await userCol.findOne(filter);
-  res.send({ result, user });
+  const wishlist = await wishlistCol.findOne(filter);
+  res.send({ result, user, wishlist });
 });
 
 // json web token handling
@@ -85,26 +96,57 @@ app.post("/jwt", async (req, res) => {
   const user = req.body;
   const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: "5h" });
   const userData = await userCol.findOne(user);
+  const wishlist = await wishlistCol.findOne(user);
   res
     .cookie(`b10a11token`, token, { httpOnly: true, secure: false })
-    .send(userData);
+    .send({ userData, wishlist });
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie(`b10a11token`, { httpOnly: true, secure: false }).send({success: true});
+  res
+    .clearCookie(`b10a11token`, { httpOnly: true, secure: false })
+    .send({ success: true });
 });
 
 // blog calls
 
-app.get("/blogs", async (req, res)=>{
+app.get("/blogs", async (req, res) => {
   const cursor = blogCol.find();
   const result = await cursor.toArray();
   res.send(result);
-})
+});
 
 app.post("/newblog", async (req, res) => {
   const newBlog = req.body;
   console.log(`adding new blog with data`, newBlog.title);
   const result = await blogCol.insertOne(newBlog);
   res.send(result);
+});
+
+// wishlist codes
+
+app.post("/newwishlist", async (req, res) => {
+  const filter = { uid: req.body.uid };
+  const options = { upsert: false };
+  const newWishlist = { uid: req.body.uid, wishlist: req.body.wishlist };
+  const wishlistData = await wishlistCol.insertOne(newWishlist);
+  const updatedUser = {
+    $set: {
+      wishlistId: wishlistData.insertedId,
+    },
+  };
+  const result = await userCol.updateOne(filter, updatedUser, options);
+  const wishlist = await wishlistCol.findOne(filter);
+  const user = await userCol.findOne(filter);
+  console.log(result, wishlistData);
+  res.send({ result, wishlistData, wishlist, user });
+});
+
+app.put("/addtowishlist", async (req, res) => {
+  const filter = { uid: req.body.uid };
+  const updatedWishlist = { $set: { wishlist: req.body.wishlist } };
+  const options = { upsert: false };
+  const result = await wishlistCol.updateOne(filter, updatedWishlist, options);
+  const newWishlist = await wishlistCol.findOne(filter);
+  res.send({ result, newWishlist })
 });
